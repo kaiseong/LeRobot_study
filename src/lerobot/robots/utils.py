1b847c3fdfc652150561f5dc301a957e3f3470e1
giseong <kaiseong4@naver.com>
2025-12-24 13:56:23 +0900

    empty

--- src/lerobot/robots/utils.py
+++ src/lerobot/robots/utils.py
@@ -64,6 +64,10 @@ def make_robot_from_config(config: RobotConfig) -> Robot:
         from tests.mocks.mock_robot import MockRobot
 
         return MockRobot(config)
+    elif config.type == "piper_follower":
+        from .piper_follower import PIPERFollower
+
+        return PIPERFollower(config)
     else:
         try:
             return cast(Robot, make_device_from_device_class(config))
